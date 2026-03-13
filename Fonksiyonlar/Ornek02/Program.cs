a2056fca9187307ff333143c1bd59fed44da2ddd
İsmail Karaman <ismailkaraman@msn.com>
2026-03-13 15:20:06 +0300

    ilk

---
+++ Fonksiyonlar/Ornek02/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ornek02
+{
+    internal class Program
+    {
+        /* Örnek: Klavyeden girilen 2 adet Sayının büyük olanını ekrana yazan programı oluşturunuz.
+         * 
+         * Not: Büyük olan sayıyı bulan işlemi bir fonksiyon tarafından yapılacaktır.(Geriye değer döndüren metod)
+         */
+        static void Main(string[] args)
+        {
+            Console.Write("1. Sayıyı Girin....:");
+            int sayi1 = int.Parse(Console.ReadLine());
+            Console.Write("2. Sayıyı Girin....:");
+            int sayi2 = int.Parse(Console.ReadLine());
+            Console.WriteLine("Girmiş Olduğunuz Bu Sayılardan Büyük Olanı " + BuyukSayi(sayi1, sayi2) + " dir.");
+            Console.ReadKey();
+
+        }
+        static int BuyukSayi(int gs1, int gs2)
+        {
+
+            if (gs1 > gs2)
+            {
+                return gs1;
+            }
+            else
+            {
+                return gs2;
+            }
+
+            //return gs1 > gs2 ? gs1 : gs2;
+
+        }
+
+    }
+}
---
+++ Fonksiyonlar/Ornek03/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ornek03
+{
+    internal class Program
+    {
+        /* Örnek: Klavyeden girilen bir sayının tek mi çift mi olduğunu ekrana yazan programı oluşturunuz.
+         * 
+         * Not: Tek mi çift mi olduğunu bulan işlemi bir fonksiyon tarafından yapılacaktır.(Geriye değer döndüren metod)
+         */
+        static void Main(string[] args)
+        {
+            Console.Write("Bir Sayı Girin....:");
+            int sayi = int.Parse(Console.ReadLine());
+            Console.WriteLine("Girmiş Olduğunuz " + sayi + " Sayısı " + TekCiftKontrol(sayi) + " dir.");
+            Console.ReadKey();
+            //Console.WriteLine("Girmiş Olduğunuz {0} sayısı {1} dir.", sayi, TekCiftKontrol(sayi));
+        }
+
+        static string TekCiftKontrol(int gelensayi)
+        {
+            //if ((gelensayi % 2) == 0)
+            //{
+            //    return "Çift";
+            //}
+            //else
+            //{
+            //    return "Tek";
+
+            //}
+            return gelensayi%2 == 0 ? "Çift" : "Tek";
+        }
+
+    }
+}
