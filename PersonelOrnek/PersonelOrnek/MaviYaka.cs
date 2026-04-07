24b32b6595c1ff771a39d78579c2eadc006d765d
İsmail KARAMAN <karaman@karatekin.edu.tr>
2026-04-08 01:17:05 +0300

    PersonelOrnek

---
+++ PersonelOrnek/PersonelOrnek/MaviYaka.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace PersonelOrnek
+{
+    internal abstract class MaviYaka : Personel
+    {
+        protected string vardiya;
+        protected decimal katsayi = 1;
+
+        public MaviYaka()
+        {
+
+        }
+        public MaviYaka(string name, string surname, int identity, string shift) : base(name, surname, identity)
+        {
+            this.adi = name;
+            this.soyadi = surname;
+            this.sicilno = identity;
+            this.vardiya = shift.ToUpper();
+            this.maas = 50000M;
+        }
+        public void MaasHesapla()
+        {
+            maas = maas * katsayi;
+        }
+
+        public void VardiyaDegistir()
+        {
+            if (vardiya == "GECE")
+            {
+                vardiya = "GÜNDÜZ";
+            }
+            else
+            {
+                vardiya = "GECE";
+            }
+            Console.WriteLine("Güncel Vardiyanız...: " + vardiya);
+        }
+    }
+}
