e2ea9a79aeddb70464ed4d095a172c7e0cfcea90
İsmail Karaman <ismailkaraman@msn.com>
2026-05-06 15:12:20 +0300

    ListeIslemleri

---
+++ ListeIslemleri/KitaplarOrnek/Kitap.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace KitaplarOrnek
+{
+    internal class Kitap
+    {
+        public string Adi { get; set; }
+        public string Yazar { get; set; }
+        public string YayinEvi { get; set; }
+        public int SayfaSayisi { get; set; }
+        public string Kategori { get; set; }
+        public int BasimYili { get; set; }
+        public decimal Fiyat { get; set; }
+
+        public Kitap(string adi, string yazar, string yayinevi, int sayfasayisi, string kategori, int basimyili, decimal fiyat)
+        {
+            this.Adi = adi;
+            this.Yazar = yazar;
+            this.YayinEvi = yayinevi;
+            this.SayfaSayisi = sayfasayisi;
+            this.Kategori = kategori;
+            this.BasimYili = basimyili;
+            this.Fiyat = fiyat;
+        }
+
+        public void Yazdir()
+        {
+            Console.WriteLine($"{Adi,-35}  {Yazar,-25}  {YayinEvi,-30}  {SayfaSayisi,4}  {Kategori,-15}  {BasimYili,4}  {Fiyat.ToString("C2"),6}");
+        }
+    }
+}
---
+++ ListeIslemleri/KitaplarOrnek/Program.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace KitaplarOrnek
+{
+    internal class Program
+    {
+        static List<Kitap> Kitaplar = KitapListe.ListeyiGetir();
+
+        static void Main(string[] args)
+        {
+            
+            Console.OutputEncoding = Encoding.UTF8;
+           
+            AnaMenu();
+        }
+
+        static void Listele()
+        {
+            Console.Clear();
+            foreach (Kitap ktp in Kitaplar)
+            {
+                ktp.Yazdir();
+            }
+            Console.WriteLine("\nListedeki Kitapların Sayısı..: " + Kitaplar.Count);
+            Console.Write("\nDevam Etmek İçin Bir Tuşa Basın....");
+            Console.ReadKey();
+
+        }
+
+        static void AnaMenu()
+        {
+            while (true)
+            {
+                Console.Clear();
+                
+                Console.WriteLine("Kitap İşlemleri");
+                Console.WriteLine("1 - Kitap Ekle");
+                Console.WriteLine("2 - Kitapları Listele");
+                Console.WriteLine("3 - Kitap Sil");
+                Console.WriteLine("4 - Kitap Sorgula");
+                Console.WriteLine("5 - Kitap Kategorileri Listesi");
+                Console.WriteLine("6 - Çıkış");
+                Console.Write("\nSeçiminiz...: ");
+                int secim = int.Parse(Console.ReadLine());
+
+                if (secim == 2)
+                {
+                    Listele();
+                }
+                else if (secim == 6)
+                {
+                    Console.Write("\nProgram Sonu...");
+                    Thread.Sleep(2000);
+                    break;
+                }
+                else
+                {
+                    Console.Clear();
+                    Console.WriteLine("Yanlış Bir Menü Seçimi Yaptınız. Ana Menüye Yönlendirileceksiniz..");
+                    Thread.Sleep(2000);
+
+                }
+            }
+        }
+    }
+}
